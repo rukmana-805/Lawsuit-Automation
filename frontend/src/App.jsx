6785075a097403f739e7ffb30a81055da272d5c9
rukmana-805 <rukmanabehera805@gmail.com>
2026-08-05 22:40:59 +0530

    Tailwind Set-Up Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1>Hii</h1>
-      </div>
+      <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind CSS Working 
+      </h1>
+    </div>
     </>
   )
 }
